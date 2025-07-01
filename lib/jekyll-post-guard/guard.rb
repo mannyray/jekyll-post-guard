52a6e7ef38375b1f89349c218b302ac8972927d3
Stan Z <nil>
2025-07-01 12:53:24 -0500

    Created functioning activity

--- lib/jekyll-post-guard/guard.rb
+++ lib/jekyll-post-guard/guard.rb
@@ -8,7 +8,8 @@ module Jekyll
             
             styling_html_content_text = File.read( File.join(plugin_dir,"style.html" ))
             header_html_content_text = File.read( File.join(plugin_dir,"header.html" ))
-            lock_html = File.read( File.join(plugin_dir,"lock.html" ))
+            
+            
             text = header_html_content_text + styling_html_content_text + text
 			doc = Nokogiri::HTML(text)
             counter = 0
@@ -18,53 +19,48 @@ module Jekyll
                 #<!--lock:{data:directory}-->
                 arguments = html_text.each_line.detect { |line| !line.strip.empty? }
                 
-                lock_html_copy = lock_html
-                if arguments.include?("<!--lock:")
+                lock_html_copy = ""
+                activity_html = ""
+                if arguments.include?("<!--lock:") # should always be included - check?
                     json_text = arguments[/<!--lock:(\{.*?\})-->/, 1]
                     parsed_data = JSON.parse(json_text) if json_text
                     #print parsed_data
                     custom_lock_dir = File.join(Jekyll.configuration({})['lock_dir'],parsed_data["data"])
+                    
                     intro_file_html = File.read( File.join(custom_lock_dir , "intro/lock.html" ))
-                    
-                    lock_assets = Dir.children(File.join(custom_lock_dir,"intro"))
+                    lock_assets_dir = File.join(custom_lock_dir,"intro")
                     # move the lock_assets to assets
+                    lock_html_copy = intro_file_html.gsub("<img src=\"","<img src=\"/"+lock_assets_dir+"/")
                     
-                    # should be http://localhost:4000 -> actual site URL and append lock directory
-                    
-                    lock_html_copy = intro_file_html.gsub("<img src=\"","<img src=\"http://localhost:4000/")
+                    activity_html = File.read( File.join(custom_lock_dir , "activity/activity.html" ))
                 end
                 
-                
+                # counters, in case you have multiple locks
                 lock_html_sub = lock_html_copy.gsub("unlockBox", "unlockBox"+counter.to_s)
-                
-                # TODO: localhost
-                
+                                
                 
                 
                 obfuscated_html = Base64.strict_encode64(html_text)
                 
-                # TODO - in some sort of function
                 lock_html_sub = lock_html_sub.sub("{/*replace*/}","{\
-                          document.getElementById('locked-#{counter}').style.display = 'block';\
+                          document.getElementById('activity-#{counter}').style.display = 'block';\
                           document.getElementById('lock-#{counter}').style.display = 'none';\
-                          el = document.getElementById('locked-#{counter}');\
-                          console.log(atob(\"#{obfuscated_html}\"));\
-                          el.innerHTML = atob(\"#{obfuscated_html}\");\
-                          console.log(window.location.origin);\
                     }")
-                                         
-                
-                #function unlockBox() { document.getElementById('redBox').style.display = 'block';}
-
-                
-                #<script>
-                #document.write(atob("PHA+dGhpcyBpcyBhIHRlc3Q8L3A+"));
-                #</script>
-                
-                            
+                    
+                activity_html_sub = activity_html.sub("{/*replace*/}","{\
+                          document.getElementById('locked-#{counter}').style.display = 'block';\
+                          document.getElementById('activity-#{counter}').style.display = 'none';\
+                          el = document.getElementById('locked-#{counter}');\
+                          el.innerHTML = atob(\"#{obfuscated_html}\");\
+                    }")
+                activity_html_sub = activity_html_sub.gsub("unlock_content_button","disabled_button-#{counter}")
+                        
                 lock_added = <<-HTML
                 <div id="lock-#{counter}" class="lock_warning">
                 #{lock_html_sub}
+                </div>
+                <div id="activity-#{counter}" class="lock_activity">
+                #{activity_html_sub}
                 </div>
                 <div id="locked-#{counter}" class="locked_off">
                 </div>
--- test_site/_posts/2022-11-12-welcome-to-jekyll.markdown
+++ test_site/_posts/2022-11-12-welcome-to-jekyll.markdown
@@ -13,14 +13,9 @@ You’ll find this post in your `_posts` directory. Go ahead and edit it and re-
 
 Jekyll requires blog post files to be named according to the following format:
 
-<!--lock_end-->
-
 `YEAR-MONTH-DAY-title.MARKUP`
 
 Where `YEAR` is a four-digit number, `MONTH` and `DAY` are both two-digit numbers, and `MARKUP` is the file extension representing the format used in the file. After that, include the necessary front matter. Take a look at the source for this post to get an idea about how it works.
-
-<!--lock_start-->
-
 
 Jekyll also offers powerful support for code snippets:
 
